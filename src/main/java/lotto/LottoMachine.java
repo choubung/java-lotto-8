225379b34d6b3f307aed9ba6f1211b013fa26e75
choubung <choubung@sookmyung.ac.kr>
2025-11-02 11:33:51 +0900

    feat(LottoMachine): 로또 발매 및 발행 내역 출력 구현

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoMachine {
+    static final int LOTTO_PRICE = 1000;
+
+    public void BuyLotto() {
+        try {
+            int amount = Integer.parseInt(Console.readLine());
+
+            validatePurchaseAmount(amount);
+
+            int lottoCount = amount / LOTTO_PRICE;
+            System.out.println(lottoCount + "개를 구매했습니다.");
+            ReleaseLotto(lottoCount);
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 정수여야 합니다.");
+        }
+    }
+
+    private void validatePurchaseAmount(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000의 배수여야 합니다.");
+        }
+    }
+
+    public void ReleaseLotto(int lottoCount) {
+        while (lottoCount-- > 0) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            String numbersStr = numbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+
+            System.out.println("[" + numbersStr + "]");
+        }
+    }
+}
