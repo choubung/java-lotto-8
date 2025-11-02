8768285878347c3561db0f26df29eec7742a9611
choubung <choubung@sookmyung.ac.kr>
2025-11-02 15:35:52 +0900

    feat(lottoRank): 당첨자 관리를 위한 LottoRank Enum 작성

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,4 +1,51 @@
 package lotto;
 
 public enum LottoRank {
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원)"), // 일치 개수, 상금, 메시지
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(5, 1_500_000, "5개 일치 (1,500,000원)"),
+    FOURTH(4, 50_000, "4개 일치 (50,000원)"),
+    FIFTH(3, 5_000, "3개 일치 (5,000원)"),
+    LOSE(0, 0, "");
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final String message;
+
+    LottoRank(int matchCount, int prizeMoney, String message) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.message = message;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static LottoRank valueOf(int matchCount, boolean hasBonus){
+        if (matchCount == 6){
+            return FIRST;
+        }
+        if (matchCount == 5 && hasBonus){
+            return SECOND;
+        }
+        if (matchCount == 5){
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3){
+            return FIFTH;
+        }
+        return LOSE;
+    }
 }
