1a2d9d0c2d6dbba7157c534924d5205c09fe11eb
choubung <choubung@sookmyung.ac.kr>
2025-11-02 15:33:29 +0900

    feat(Lotto): 로또 번호 중복 예외 처리)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -21,6 +21,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다. ");
+        }
     }
 
     @Override
