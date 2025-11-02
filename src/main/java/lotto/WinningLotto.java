1eb53e5c79edbc8824338d1d9fd71b1fdb539547
choubung <choubung@sookmyung.ac.kr>
2025-11-02 15:36:54 +0900

    feat(lotto): 당첨 로또 관리(번호와 보너스번호)를 위한 클래스 작성

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,4 +1,24 @@
 package lotto;
 
+import java.util.List;
+
 public class WinningLotto {
+    private Lotto lotto;
+    private int bonusNumber;
+
+    public WinningLotto() {
+    }
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.lotto = new Lotto(winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
