f320f5c931dc79be04750fe8d88179578c5921ee
choubung <choubung@sookmyung.ac.kr>
2025-11-03 15:21:46 +0900

    test(lotto): 로또 테스트 추가 로또 생성시 로또 번호 정렬 여부를 테스트함.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,17 +1,23 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
-    // 로또 정렬되어 저장되는지
-    // toString 정해진 양식으로 출력 되는지
+    @DisplayName("로또를 생성할 때 로또 번호를 정렬한다.")
+    @Test
+    void 로또_정렬_테스트(){
+        Lotto lotto = new Lotto(List.of(7, 2, 36, 18, 45, 9));
+        assertThat(lotto.getNumbers()).isSorted();
+    }
 
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
