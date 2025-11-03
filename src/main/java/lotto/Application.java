75374630d1e44dcaf9accb585118b6cfcb12ba68
choubung <choubung@sookmyung.ac.kr>
2025-11-03 11:13:04 +0900

    refactor(manager): 클래스 이름을 WinningLottoManager -> WinningSystemManager로 변경 원래 이름은 단순히 WinningLotto를 관리하는 것으로 보일 여지가 있었음. 그러나 본 클래스의 기능은 당첨 시스템 (관리)에 가까움. 따라서 이름을 WinningSystemManager로 리팩터링함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,9 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         LottoMachine machine = new LottoMachine();
-        WinningLottoManager winningLottoManager = new WinningLottoManager();
+        WinningSystemManager winningLottoManager = new WinningSystemManager();
 
         machine.BuyLotto(); // 로또 구매
         winningLottoManager.saveWinningLotto(); // 당첨 번호 입력 받아 저장
--- src/main/java/lotto/WinningSystemManager.java
+++ src/main/java/lotto/WinningSystemManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class WinningLottoManager {
+public class WinningSystemManager {
     private WinningLotto winningLotto;
     private double profit;
     private Map<LottoRank, Integer> rankCounts = new HashMap<LottoRank, Integer>();
