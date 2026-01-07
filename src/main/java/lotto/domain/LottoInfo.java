88c0acda5332a8f0869bd9e4b4894e11b99badf1
choubung <choubung@sookmyung.ac.kr>
2026-01-07 22:12:25 +0900

    feat: LottoInfo 작성

--- src/main/java/lotto/domain/LottoInfo.java
+++ src/main/java/lotto/domain/LottoInfo.java
@@ -1,4 +1,37 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
+import static java.util.spi.ToolProvider.findFirst;
+
 public enum LottoInfo {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int bingo;
+    private final int prize;
+
+    LottoInfo(int bingo, int prize) {
+        this.bingo = bingo;
+        this.prize = prize;
+    }
+
+    public static LottoInfo from(int input, boolean bonus) {
+        if (input == 5 && bonus) {
+            return LottoInfo.SECOND;
+        }
+
+        return Arrays.stream(values())
+                .filter(lottoInfo -> lottoInfo.bingo == input)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
