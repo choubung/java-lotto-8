7648ebf6614c0729ece72e255c7902918062df3b
choubung <choubung@sookmyung.ac.kr>
2026-01-07 21:36:09 +0900

    feat: 입력 기능 구현 로또 구입 금액 입력 당첨 번호 입력 보너스 번호 입력

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,4 +1,28 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
+    private static final Pattern LOTTO_NUM_PATTERN = Pattern.compile("([0-9]+)\\,([0-9]+)\\,([0-9]+)\\,([0-9]+)\\,([0-9]+)\\,([0-9]+)");
+
+    public static List<Integer> parse(String input) {
+        List<Integer> nums = new ArrayList<>();
+        Matcher matcher = LOTTO_NUM_PATTERN.matcher(input);
+
+        if (matcher.matches()) {
+            try {
+                for (int i = 1; i < nums.size() + 1; i++ ){
+                    nums.add(Integer.parseInt(matcher.group(i).trim()));
+                }
+
+                return nums;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("로또 번호는 숫자여야 합니다.");
+            }
+        }
+        throw new IllegalArgumentException("입력 형식이 올바르지 않습니다.");
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,58 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.Parser;
+import lotto.utils.Validator;
+
+import java.util.List;
+
 public class InputView {
+    public int readCash() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        int cash = Validator.validateAndParseCash(input);
+        return cash;
+    }
+
+    public List<Integer> readWinLottoNum() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        List<Integer> winLottoNums = Parser.parse(input);
+
+        return winLottoNums;
+    }
+
+    public int readWinBonusNum() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        int bonus = Validator.parseToInt(input);
+        return bonus;
+    }
+
+//    // 1. 기본 문자열 입력
+//    public String readString() {
+//        System.out.println("문자열을 입력해 주세요.");
+//        String input = Console.readLine();
+//        Validator.validateHasText(input);
+//        return input;
+//    }
+//
+//    // 2. 숫자 입력 (자동 형변환)
+//    public int readNumber() {
+//        System.out.println("숫자를 입력해 주세요.");
+//        String input = Console.readLine();
+//        Validator.validateIsNumeric(input);
+//        return Integer.parseInt(input);
+//    }
+//
+//    // 3. 쉼표 구분 입력 (리스트 반환)
+//    public List<String> readList() {
+//        System.out.println("쉼표로 구분하여 입력해 주세요.");
+//        String input = Console.readLine();
+//        Validator.validateHasText(input);
+//
+//        return Arrays.stream(input.split(","))
+//                .map(String::trim)
+//                .collect(Collectors.toList());
+//    }
 }
