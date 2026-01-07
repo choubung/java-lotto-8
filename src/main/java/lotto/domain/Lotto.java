5b1142930fa51d7f3c48e03d9fef217aeeed4b2d
choubung <choubung@sookmyung.ac.kr>
2026-01-07 22:01:58 +0900

    feat(lotto): 로또 유효성 검사 메소드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,17 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateInRange(numbers);
+
         this.numbers = numbers;
     }
 
@@ -16,10 +21,19 @@ public class Lotto {
         }
     }
 
-    // TODO: 중복 확인
-    private void validateDuplicate() {
-
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("중복된 값이 존재합니다.");
+        }
     }
 
-    // TODO: 범위 내의 숫자인지
+    private void validateInRange(List<Integer> numbers) {
+        for (int num : numbers) {
+            if (num < 1 || num > 45) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+
+    }
 }
